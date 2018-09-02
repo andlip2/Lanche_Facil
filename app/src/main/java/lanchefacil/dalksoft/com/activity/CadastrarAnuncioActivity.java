3e72ad5e90d93406934dd710db7aedf6fda76a02
Anderson Felipe <anderson.felipe235@gmail.com>
2018-09-02 13:30:24 -0300

    Solicitação de acesso ao GPS

--- app/src/main/java/lanchefacil/dalksoft/com/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/lanchefacil/dalksoft/com/activity/CadastrarAnuncioActivity.java
@@ -1,24 +1,79 @@
 package lanchefacil.dalksoft.com.activity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.support.v4.content.ContextCompat;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import lanchefacil.dalksoft.com.Manifest;
+import java.io.IOException;
+import java.util.List;
 import lanchefacil.dalksoft.com.R;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
 
 
+    private TextView tv;
+    private LocationManager mLocalizacao;
+    protected Location localizacao;
+    private Address endereco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
 
-        ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS);
+
+        tv = findViewById(R.id.tvtvtv);
+        double latitude =0.0;
+        double longitude = 0.0;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+    }else {
+            mLocalizacao = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            localizacao = mLocalizacao.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+     if (localizacao != null) {
+            longitude = localizacao.getLongitude();
+            latitude = localizacao.getLatitude();
+     }
+
+     try {
+            endereco = buscarEndereco(latitude, longitude);
+            tv.setText("Cidade: " + endereco.getLocality());
+     } catch (IOException e) {
+         e.printStackTrace();
+     }
+
+
     }
 
+    private Address buscarEndereco(double latitude, double longitude) throws IOException {
+        Geocoder geocoder;
+        Address address = null;
+        List<Address> addresses;
+
+        geocoder = new Geocoder(getApplicationContext());
+
+        addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+        if (addresses.size() >0) {
+            address = addresses.get(0);
+        }
+        return address;
+    }
+    private void alerta (String texto) {
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
+    }
 
 }
--- app/src/main/java/lanchefacil/dalksoft/com/activity/PrincipalActivity.java
+++ app/src/main/java/lanchefacil/dalksoft/com/activity/PrincipalActivity.java
@@ -1,18 +1,26 @@
 package lanchefacil.dalksoft.com.activity;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
@@ -21,12 +29,18 @@ import com.google.firebase.auth.FirebaseUser;
 
 import lanchefacil.dalksoft.com.R;
 import lanchefacil.dalksoft.com.helper.ConfigFireBase;
+import me.drakeet.materialdialog.MaterialDialog;
 
 public class PrincipalActivity extends AppCompatActivity
 
+
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
     private FirebaseAuth autenticacao = ConfigFireBase.getFirebaseAuth();
+    public static final String TAG = "LOG";
+    public static final int REQUEST_PERMISSIONS_CODE = 128;
+    private MaterialDialog mMaterialDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +49,17 @@ public class PrincipalActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        callAccessLocation();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent i = new Intent(PrincipalActivity.this, CadastrarAnuncioActivity.class);
+                startActivity(i);
             }
         });
 
@@ -141,6 +160,45 @@ public class PrincipalActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void callAccessLocation() {
+        Log.i (TAG,"callAccessLocation()");
+
+        if( ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ){
+
+            if( ActivityCompat.shouldShowRequestPermissionRationale( this, Manifest.permission.ACCESS_FINE_LOCATION ) ){
+                callDialog( "É preciso a permission ACCESS_FINE_LOCATION para apresentação dos eventos locais.", new String[]{Manifest.permission.ACCESS_FINE_LOCATION} );
+            }
+            else{
+                ActivityCompat.requestPermissions( this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSIONS_CODE );
+            }
+        }
+        else{
+
+        }
+    }
+
+    private void callDialog( String message, final String[] permissions ){
+        mMaterialDialog = new MaterialDialog(this)
+                .setTitle("Permission")
+                .setMessage( message )
+                .setPositiveButton("Ok", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        ActivityCompat.requestPermissions(PrincipalActivity.this, permissions, REQUEST_PERMISSIONS_CODE);
+                        mMaterialDialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Cancel", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mMaterialDialog.dismiss();
+                    }
+                });
+        mMaterialDialog.show();
+    }
+
     private void alerta (String texto) {
         Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
     }
