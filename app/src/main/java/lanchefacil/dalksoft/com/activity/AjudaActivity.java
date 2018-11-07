84b3d16f1477c804160103260180154d332e666f
Anderson Felipe <anderson.felipe235@gmail.com>
2018-11-07 14:44:17 -0300

    Alterações no Layout da tela de ajuda

--- app/src/main/java/lanchefacil/dalksoft/com/activity/AjudaActivity.java
+++ app/src/main/java/lanchefacil/dalksoft/com/activity/AjudaActivity.java
@@ -2,10 +2,13 @@ package lanchefacil.dalksoft.com.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import lanchefacil.dalksoft.com.R;
 
 public class AjudaActivity extends AppCompatActivity {
+
+    private TextView pergunta1, pergunta2, pergunta3, pergunta4, pergunta5, pergunta6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +16,17 @@ public class AjudaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ajuda);
 
         getSupportActionBar().setTitle("Ajuda");
+
+        inicializarComponentes();
+    }
+
+    private void inicializarComponentes() {
+        pergunta1 = findViewById(R.id.textAjudaP1);
+        pergunta2 = findViewById(R.id.textAjudaP2);
+        pergunta3 = findViewById(R.id.textAjudaP3);
+        pergunta4 = findViewById(R.id.textAjudaP4);
+        pergunta5 = findViewById(R.id.textAjudaP5);
+        pergunta6 = findViewById(R.id.textAjudaP6);
     }
 }
+
